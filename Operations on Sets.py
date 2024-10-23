8cf207adbb9ec681994b29cd90f4fc8d75bcbdcd
sreInk <srinknok@gmail.com>
2024-10-23 09:01:26 +0530

    first commit

---
+++ Operations on Sets.py
@@ -0,0 +1,18 @@
+my = {1,3,5}
+print(my)
+
+my = {1.0,'Hello',(1,3,5)}
+print(my)
+
+my = {1,2,3,4,3,4,2,1}
+print(my)
+
+my = {1,2,4,5}
+print(my)
+
+ns = set([0,1,2,3,4])
+print("Real Number")
+print(ns)
+ns.pop()
+print("The edited")
+print(ns)
