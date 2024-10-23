70184c74050f0367d1f35602f1497617f1dcc8ab
sreInk <srinknok@gmail.com>
2024-10-23 09:09:47 +0530

    2w

---
+++ Intersectiona And Linera.py
@@ -0,0 +1,25 @@
+c = {"Blue","Yellow"}
+d = {"Blue","Green"}
+print("The Original Vaule:")
+print(c)
+print(d)
+print("The Edited Vaule:")
+z = c.intersection(d)
+print(z)
+z1 = c.union(d)
+print(z1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
